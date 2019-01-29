81bc6fd11e33e31cce9513962c11a8b3cbfa0af4
charlescqian <charles.miffy@gmail.com>
2019-01-29 00:30:01 -0800

    Added Pages and Components Folder to Repo

---
+++ src/Components/Navbar.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from "../logo.svg";
+import * as Scroll from 'react-scroll';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
+class Navbar extends Component {
+    scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+
+    render() {
+        return (
+            <nav className="navbar" id="navbar" style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <div className="navbar-content">
+                    <img
+                        src={logo}
+                        className="navbar-logo"
+                        alt="Logo"
+                        onClick={this.scrollToTop}
+                    />
+                    <ul className="navbar-items">
+                        <li className="navbar-item">
+                            <Link
+                                activeClass="active"
+                                to="about-page"
+                                spy={true}
+                                smooth={true}
+                                offset={50}
+                                duration={500}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link
+                                activeClass="active"
+                                to="projects-page"
+                                spy={true}
+                                smooth={true}
+                                offset={50}
+                                duration={500}
+                            >
+                                Projects
+                            </Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link
+                                activeClass="active"
+                                to="hobbies-page"
+                                spy={true}
+                                smooth={true}
+                                offset={50}
+                                duration={500}
+                            >
+                                Hobbies
+                            </Link> 
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+export default Navbar;
---
+++ src/Pages/ProjectsPage/ProjectsPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class ProjectsPage extends Component {
+    render() {
+        return( 
+            <div className="projects-page">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce. Nulla pharetra diam sit amet. Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Nulla pellentesque dignissim enim sit. Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Eu augue ut lectus arcu. Nibh cras pulvinar mattis nunc sed blandit libero. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. At erat pellentesque adipiscing commodo elit at imperdiet. Nunc sed id semper risus in hendrerit gravida rutrum quisque. Tempus imperdiet nulla malesuada pellentesque elit eget. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Bibendum neque egestas congue quisque egestas diam in arcu cursus. Quis commodo odio aenean sed adipiscing diam donec adipiscing tristique. Ut lectus arcu bibendum at. Sed libero enim sed faucibus turpis in.
+
+Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Nulla at volutpat diam ut venenatis. At erat pellentesque adipiscing commodo elit at. Elementum nisi quis eleifend quam adipiscing vitae proin. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Diam maecenas ultricies mi eget mauris pharetra. Turpis massa sed elementum tempus. Porttitor rhoncus dolor purus non enim. Suspendisse potenti nullam ac tortor vitae purus. Eget mi proin sed libero enim sed faucibus turpis in.
+
+Odio tempor orci dapibus ultrices in iaculis nunc sed. Luctus accumsan tortor posuere ac. Scelerisque felis imperdiet proin fermentum leo vel orci porta. Ut lectus arcu bibendum at. Condimentum lacinia quis vel eros donec ac odio tempor orci. Bibendum at varius vel pharetra. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Egestas dui id ornare arcu odio. Ut tellus elementum sagittis vitae et leo duis ut diam. Habitasse platea dictumst vestibulum rhoncus est. Et malesuada fames ac turpis egestas sed tempus urna et. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Porta lorem mollis aliquam ut porttitor. Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+
+Euismod quis viverra nibh cras pulvinar mattis nunc. Amet massa vitae tortor condimentum lacinia. Ipsum dolor sit amet consectetur. Fringilla est ullamcorper eget nulla. Dictumst vestibulum rhoncus est pellentesque. Montes nascetur ridiculus mus mauris vitae. Quis viverra nibh cras pulvinar mattis nunc sed blandit. In fermentum et sollicitudin ac orci phasellus. Venenatis tellus in metus vulputate eu scelerisque felis. Lacus viverra vitae congue eu consequat ac felis donec et.
+
+Ultricies integer quis auctor elit sed vulputate. Suspendisse in est ante in nibh mauris cursus mattis molestie. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. In dictum non consectetur a erat nam. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. Ipsum suspendisse ultrices gravida dictum fusce. At tellus at urna condimentum mattis pellentesque. Diam volutpat commodo sed egestas egestas fringilla. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit. Libero enim sed faucibus turpis in eu. Eu tincidunt tortor aliquam nulla facilisi cras.                </p>
+            </div>
+        );
+    }
+}
+
+export default ProjectsPage;
